88af955cf1257d6899f39095d05d9fbb3b362494
bigetti <upgrade81@mail.ru>
2024-01-12 12:10:34 +0300

    models maded2

--- school/views.py
+++ school/views.py
@@ -15,6 +15,13 @@ def students_list(request):
     context = {'students': students}
 
     print("students_list view is called!")
+    for student in students:
+        print(f"Student: {student.name}, Teachers: {[teacher.name for teacher in student.teachers.all()]}")
+        teachers = student.teachers.all()
+        if teachers:
+            print(f"Teachers: {', '.join([teacher.name for teacher in teachers])}")
+        else:
+            print("No teachers assigned.")
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
