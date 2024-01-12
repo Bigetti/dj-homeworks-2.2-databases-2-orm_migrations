41f433da1f65912a12e46be98766dbd8f8135f63
bigetti <upgrade81@mail.ru>
2024-01-12 11:43:32 +0300

    models maded

--- school/views.py
+++ school/views.py
@@ -5,8 +5,16 @@ from .models import Student
 
 
 def students_list(request):
+
     template = 'school/students_list.html'
-    context = {}
+
+    # Получаем все объекты студентов
+    students = Student.objects.all()
+
+    # Передаем студентов в контекст представления
+    context = {'students': students}
+
+    print("students_list view is called!")
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
